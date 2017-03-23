035568c189aac3d649604c89a7a9789c99b7774c
mtafuro <tafuro@outlook.com>
2017-03-23 16:58:02 -0500

    Added win/loss condition, player guesses
    Added player guesses and win loss conditions. Still needs testing.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -9,11 +9,45 @@ dictionary.delete_if { |word| word.length >= (13) }
 game_word = dictionary.sample # define method?
 
 # Gamespace
-word_length = game_word.length
-word_length.times do |letter| print "_" end
+gamespace = game_word.gsub(/./, "_")
 
+# Game loop
 # Player guess
-player_guess = gets
-puts player_guess
+begin
+  player_guess = gets.chomp!
 
-word_length.include?(player_guess)
+  # Player attempted letters
+  player_attempts = Array.new
+  player_attempts.push(player_guess)
+  player_attempts_count = player_attempts.length
+
+  # Limit to one character a-z
+  if player_guess =~ /[a-z]/ || /[A-Z]/
+    puts "Please enter a character between A and Z."
+  elsif player_guess.length != 1
+    puts "Please enter just one character."
+  elsif player_attempts.each { |letter| letter == player_guess}
+    puts "Please enter a letter you haven't yet picked."
+  end
+
+  # Player guess correct?
+  if game_word.include?(player_guess)
+    puts "#{player_guess} is in the hidden word!"
+    guess_loc = game_word.index(player_guess) # Player guess string location
+    gamespace.insert(guess_loc, player_guess) # Replace gamespace "_" with correct letter
+    puts gamespace
+  else
+    puts "#{player_guess} is not in the hidden word... I'm so sorry for you."
+  end
+end while
+
+class WinLoseCondition
+  def initialize(player_attempts_count)
+    if player_attempts_count > 6
+      puts "Your Hangyman is hung... This is bad news."
+      return # ?true?
+    else gamespace.includes?("_") == false
+      puts "You have saved Hangyman! You must feel like such a hero."
+    end
+  end
+end
