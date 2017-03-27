740c96eb3e47e4d589b93053021102d5e3f56fc3
mtafuro <tafuro@outlook.com>
2017-03-27 13:10:11 -0500

    Updated to separate methods

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,55 +1,91 @@
-# Load in full dictionary
-full_dictionary = File.read('5desk.txt')
-dictionary = full_dictionary.scan(/\w+/) #convert to array
-dictionary.delete_if { |word| word =~ (/[A-Z]\w+/) } # eliminate words beginning with a capital letter
-dictionary.delete_if { |word| word.length <= (4) }
-dictionary.delete_if { |word| word.length >= (13) }
+class Hangyman
+  attr_accessor :game_word, :blanks, :game_round
 
-# Select random word
-game_word = dictionary.sample # define method?
+  def initialize
+    new_word
+    gamespace
+    attempts_array
+    game_round
+  end
+
+  # Load in full dictionary
+  def new_word
+    full_dictionary = File.read('5desk.txt')
+    dictionary = full_dictionary.scan(/\w+/) #convert to array
+    dictionary.delete_if { |word| word =~ (/[A-Z]\w+/) } # eliminate words beginning with a capital letter
+    dictionary.delete_if { |word| word.length <= (4) }
+    dictionary.delete_if { |word| word.length >= (13) }
+    @game_word = ""
+    @game_word = dictionary.sample
+  end
 
 # Gamespace
-gamespace = game_word.gsub(/./, "_")
+def gamespace
+  @blanks = @game_word.gsub(/./, "_")
+end
+
+def attempts_array
+  @player_attempts = Array.new
+  @player_attempts_count = @player_attempts.length
+end
 
 # Game loop
 # Player guess
-begin
-  player_guess = gets.chomp!
-
-  # Player attempted letters
-  player_attempts = Array.new
-  player_attempts.push(player_guess)
-  player_attempts_count = player_attempts.length
-
-  # Limit to one character a-z
-  if player_guess.match(/[[:alpha:]]/) == nil
-    puts "Please enter a character between A and Z."
-  elsif player_guess.length != 1
-    puts "Please enter just one character."
-  elsif player_attempts.each { |letter| letter == player_guess}
-    puts "Please enter a letter you haven't yet picked."
+# --------------------------------------- seperate this method
+  def game_round
+    puts @blanks
+    puts "Your word has #{@game_word.length} letters."
+    puts "Here are your attempts #{@player_attempts}. Make a guess!"
+    @player_guess = gets.chomp!
+    # Cannot repeat letters.
+    if @player_attempts.include?(@player_guess)
+      puts "Please enter a letter you haven't yet picked."
+      game_round
+    # Limit to one character, a-z
+    elsif @player_guess.match(/[[:alpha:]]/) == nil
+      puts "Please enter a character between A and Z."
+      game_round
+    elsif @player_guess.length != 1
+      puts "Please enter just one character."
+      game_round
+    else
+      letter_correct?
+    end
   end
+# --------------------------------------- seperate this method
 
-  # Player guess correct?
-  if game_word.include?(player_guess)
-    puts "#{player_guess} is in the hidden word!"
-    guess_loc = game_word.index(player_guess) # Player guess string location
-    gamespace.insert(guess_loc, player_guess) # Replace gamespace "_" with correct letter
-    puts gamespace
-  else
-    puts "#{player_guess} is not in the hidden word... I'm so sorry for you."
+# Player guess correct?
+# --------------------------------------- seperate this method
+  def letter_correct?
+    if @game_word.include?(@player_guess)
+      puts "\'#{@player_guess}\' is in the hidden word!"
+      guess_loc =
+        each
+        @game_word.index(@player_guess) # Player guess string location
+      @blanks.insert(guess_loc, @player_guess) # Replace blanks "_" with correct letter
+      @player_attempts.push(@player_guess)
+      game_round
+    else
+      puts "\'#{@player_guess}\' is not in the hidden word... I'm so sorry for you."
+      @player_attempts.push(@player_guess)
+      game_round
+    end
   end
-end while WinLoseCondition == true || false
+# --------------------------------------- seperate this method
 
-class WinLoseCondition
-  def initialize(player_attempts_count)
-    if player_attempts_count > 6
+# Win / Lose conditions
+# --------------------------------------- seperate this method
+  def win_lose?
+    if @player_attempts_count > 6
       puts "Your Hangyman is hung... This is bad news."
       return true
-    elsif gamespace.includes?("_") == false
+    elsif @blanks.includes?("_") == false
       puts "You have saved Hangyman! You must feel like such a hero."
       return false
     else nil
     end
   end
 end
+# --------------------------------------- seperate this method
+
+Hangyman.new
